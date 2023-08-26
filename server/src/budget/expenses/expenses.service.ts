7a70b696bdbbc2f3e21a56f7d9614e016004c48c
KlaudiaDabrowska <kosaa96@gmail.com>
2023-08-26 14:17:27 +0200

    add totalAmount of expenses and group by categories

--- server/src/budget/expenses/expenses.service.ts
+++ server/src/budget/expenses/expenses.service.ts
@@ -8,8 +8,42 @@ import { Expense } from './expenses.entity';
 export class ExpensesService {
   constructor(@InjectRepository(Expense) private repo: Repository<Expense>) {}
 
-  getExpenses() {
-    return this.repo.find();
+  async getExpenses() {
+    const totalAmount = this.repo
+      .createQueryBuilder('expense')
+      .select('SUM(expense.amount)', 'totalAmount')
+      .where('expense.date >= :startDate', {
+        startDate: '2023-08-24T22:00:00.000Z',
+      })
+      .andWhere('expense.date <= :endDate', {
+        endDate: '2023-08-25T22:00:00.000Z',
+      })
+      .getRawOne();
+
+    const groupByCategories = this.repo
+      .createQueryBuilder('expense')
+      .select('expense.expense_category', 'expenseCategory')
+      .addSelect('SUM(expense.amount)', 'totalAmount')
+      .where('expense.date >= :startDate', {
+        startDate: '2023-08-24T22:00:00.000Z',
+      })
+      .andWhere('expense.date <= :endDate', {
+        endDate: '2023-08-26T22:00:00.000Z',
+      })
+      .groupBy('expense.expense_category')
+      .getRawMany();
+
+    const allExpenses = this.repo.find();
+
+    return Promise.all([allExpenses, totalAmount, groupByCategories]).then(
+      (values) => {
+        return {
+          expenses: values[0],
+          ...values[1],
+          groupByCategories: values[2],
+        };
+      },
+    );
   }
 
   getExpenseById(id: string) {
