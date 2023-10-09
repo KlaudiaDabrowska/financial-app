224261f90e7b858913786b15c0a68f1ad0e1cc71
KlaudiaDabrowska <kosaa96@gmail.com>
2023-10-09 14:41:40 +0200

    rewrite get savings endpoint

--- server/src/budget/savings/savings.service.ts
+++ server/src/budget/savings/savings.service.ts
@@ -9,7 +9,40 @@ export class SavingService {
   constructor(@InjectRepository(Saving) private repo: Repository<Saving>) {}
 
   getSavings() {
-    return this.repo.find();
+    const totalAmount = this.repo
+      .createQueryBuilder('saving')
+      .select('SUM(saving.amount)', 'amount')
+      .addSelect('saving.currency', 'currency')
+      .where('saving.date >= :startDate', {
+        startDate: '2023-08-10T22:00:00.000Z',
+      })
+      .andWhere('saving.date <= :endDate', {
+        endDate: '2023-08-29T22:00:00.000Z',
+      })
+      .groupBy('saving.currency')
+      .getRawMany();
+
+    const allSavings = this.repo
+      .createQueryBuilder('saving')
+      .select('saving.saving_category', 'savingCategory')
+      .addSelect('saving.amount', 'amount')
+      .addSelect('saving.currency', 'currency')
+      .addSelect('saving.date', 'date')
+      .addSelect('saving.id', 'id')
+      .where('saving.date >= :startDate', {
+        startDate: '2023-08-10T22:00:00.000Z',
+      })
+      .andWhere('saving.date <= :endDate', {
+        endDate: '2023-08-29T22:00:00.000Z',
+      })
+      .getRawMany();
+
+    return Promise.all([allSavings, totalAmount]).then((values) => {
+      return {
+        savings: values[0],
+        totalAmount: values[1],
+      };
+    });
   }
 
   getSavingById(id: string) {
