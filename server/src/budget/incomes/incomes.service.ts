2c97952fc045e8270f3c9efaa3c0e60f2fab3bf6
KlaudiaDabrowska <kosaa96@gmail.com>
2023-10-09 14:04:10 +0200

    rewrite get incomes endpoint

--- server/src/budget/incomes/incomes.service.ts
+++ server/src/budget/incomes/incomes.service.ts
@@ -9,7 +9,41 @@ export class IncomesService {
   constructor(@InjectRepository(Income) private repo: Repository<Income>) {}
 
   getIncomes() {
-    return this.repo.find();
+    const totalAmount = this.repo
+      .createQueryBuilder('income')
+      .select('SUM(income.amount)', 'amount')
+      .addSelect('income.currency', 'currency')
+      .where('income.date >= :startDate', {
+        startDate: '2023-08-26T22:00:00.000Z',
+      })
+      .andWhere('income.date <= :endDate', {
+        endDate: '2023-08-29T22:00:00.000Z',
+      })
+      .groupBy('income.currency')
+      .getRawMany();
+
+    const allIncomes = this.repo
+      .createQueryBuilder('income')
+      .select('income.income_type', 'incomeType')
+      .addSelect('income.amount', 'amount')
+      .addSelect('income.currency', 'currency')
+      .addSelect('income.date', 'date')
+      .addSelect('income.payment_type', 'paymentType')
+      .addSelect('income.id', 'id')
+      .where('income.date >= :startDate', {
+        startDate: '2023-08-26T22:00:00.000Z',
+      })
+      .andWhere('income.date <= :endDate', {
+        endDate: '2023-08-29T22:00:00.000Z',
+      })
+      .getRawMany();
+
+    return Promise.all([allIncomes, totalAmount]).then((values) => {
+      return {
+        incomes: values[0],
+        totalAmount: values[1],
+      };
+    });
   }
 
   getIncomeById(id: string) {
